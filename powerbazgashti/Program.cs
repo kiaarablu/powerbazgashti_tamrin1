f30a3500ffa958bb1dd413350c189ee0a8742e28
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-07-21 05:38:49 -0700

    Add files via upload

---
+++ powerbazgashti/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powerbazgashti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int pow (int number , int power)
+            {
+                if (power == 0)
+                {
+                    return 1; 
+                }
+                else
+                {
+                    int result = number * pow(number, power - 1);
+                    return result;
+                }
+               
+            }
+            int a  = Convert.ToInt32(Console.ReadLine());
+            int b= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(pow(a , b));
+            Console.ReadKey();
+        }
+    }
+}
